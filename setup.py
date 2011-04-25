5ae62acbed60647386162d856c95bc7a1df5522e
Paul Colomiets <pc@gafol.net>
2011-04-25 14:58:03 +0300

    Added some packaging files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='buyore',
+    version='0.1',
+    description='Package manager for Arch Linux (pacman frontend)',
+    author='Paul Colomiets',
+    author_email='pc@gafol.net',
+    url='http://github.com/tailhook/buyore',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        ],
+    packages=['buyore'],
+    scripts=['scripts/buy'],
+    requires=['pyarchive'],
+    )
